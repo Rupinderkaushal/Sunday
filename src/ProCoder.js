955addfa688a07395a2f29b558f6f22ca48e4bfa
Rupinder Kaushal <89014750+Rupinderkaushal@users.noreply.github.com>
2021-12-19 22:04:25 +0530

    third

--- src/ProCoder.js
+++ src/ProCoder.js
@@ -1,33 +1,83 @@
 import React from 'react';
 import {Form,Button,Row,Col} from 'react-bootstrap'
 const ProCoder = () => {
+  const divStyle= {
+    display:"flex",
+    alignItems:"center",
+    justifyContent:"center",
+  }
+  const onclick =(e)=>{
+    e.preventDefault()
+    const x = document.getElementById("email").value;
+    if(x == ""){
+      alert("enter email")
+      return;
+    }
+    const y = x.indexOf("@");
+      if(y == -1){
+        alert("include @")
+      return;
+      }
+    const z= x.indexOf(".com");
+      if(z == -1)
+    {
+      alert("include .com")
+      return;
+    }
+    const pass = document.getElementById('password').value;
+    if(pass ==""){
+      alert("enter password")
+      return;
+    }
+    if(/^[a-z,A-Z\b]+$/.test(pass)){
+      alert("enter only integer")
+      return;
+    }
+    
+  }
     return (
         <div>
-            <Form>
-                <Row>
-                    <Col style={{border:"2px solid green"}}  md="3">
-                    <Form.Group  className="mb-3 mt-3" controlId="formBasicEmail">
+            <Form   className='container bg-danger form'>
+                <Row style={divStyle} >
+                    <Col>
+                    <Form.Group  className="mb-3 mt-3 " controlId="formBasicEmail">
     <Form.Label  >Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
+    <Form.Control id="email" type="email" placeholder="Enter email" />
+    
   </Form.Group>
                     </Col>
-                    <Col style={{border:"2px solid green"}}  md="3">
+                    <Col>
                     <Form.Group  className="mb-3 mt-3" controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
+    <Form.Control  id="password" type="password" placeholder="Password" />
   </Form.Group>
                     </Col>
                 </Row>
-  
-
-  
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
+                <Row style={divStyle}>
+    <Col>
+    <Form.Group className="mb-3 mt-3" >
+    <Form.Label>Confirm Password</Form.Label>
+    <Form.Control className="confirmp" type="password" placeholder="Cnfirm Password" />
+    </Form.Group>
+    </Col>
+    <Col>
+    <Form.Group  className="mb-3 mt-3" >
+      <Form.Label>Enter Your Mobile No.</Form.Label>
+      <Form.Control className='mobile' type="text" placeholder="Enter Mobile No" />
+    </Form.Group>
+    </Col>
+                </Row>
+                <Row style={divStyle} >
+                  <Col>
+                  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                  <Form.Check className="checkbox" type="checkbox" label="Male" value="male" />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                  <Form.Check className="checkbox" type="checkbox" label="FeMale" value="female" />
+                  </Form.Group>
+                  </Col>
+                   </Row>
+  <Button className='container' variant="primary" type="submit" onClick={onclick} >
     Submit
   </Button>
 </Form>
